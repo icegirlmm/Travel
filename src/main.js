2d39893076aba062dadd74f1114b8f0d50b0f4ab
icegirlmm <icegirlm@163.com>
2018-07-06 18:36:06 +0800

    change

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick' // 解决移动端300毫秒点击延迟的问题
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'// 移动端的一像素物理问题
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
 fastClick.attach(document.body) // 把fastClick加载到body上
+Vue.use(VueAwesomeSwiper) /* 在vue.use(vueAwesomeSwiper,default global options)后面还可以加默认的参数{ default global options } */
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="wrapper">
+ <swiper  :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="去哪网">
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/3c/c72a1ccd4d7b2202.jpg_750x200_b88bbab4.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/6a/18f9b171a9275c02.jpg_750x200_97cd5547.jpg'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/99/716c43b486692b02.jpg_750x200_2f2c866a.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>>.swiper-pagination-bullet-active
+    background: #fff
+.wrapper
+    overflow: hidden
+    width:100%
+    height:0
+    padding-bottom: 26.67%
+    // padding-bottom 的百分比相对的是父级元素的宽度，这样使得宽高比保持一定的比例
+    // 而height的百分比是相对父级的高度
+    background: #eee
+    .swiper-img
+        width: 100%
+</style>
